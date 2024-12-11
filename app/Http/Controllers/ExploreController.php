dafa3659b42bc5bbcec18544d7518075fd9c9121
Steven4565 <stevenchristian4565@gmail.com>
2024-12-12 04:36:52 +0700

    display images

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -11,11 +11,11 @@ class ExploreController extends Controller
 {
     public function index()
     {
-        $images = [
-            ['url' => 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg', 'description' => 'Image 1', 'title' => 'image 1 title'],
-            ['url' => 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg', 'description' => 'Image 2', 'title' => 'image 2 title'],
-            // Add more images here...
-        ];
+        $images = ImagePost::all();
+        foreach ($images as $image) {
+            $image->url = Storage::url($image["url"]);
+        }
+
         return view('explore.index', compact('images'));
     }
 
