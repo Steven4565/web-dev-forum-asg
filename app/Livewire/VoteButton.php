c46c306fad8d70a6957fe7bcdc2701f08484b625
Steven4565 <stevenchristian4565@gmail.com>
2024-12-12 13:14:41 +0700

    fixed inteliphense error

--- app/Livewire/VoteButton.php
+++ app/Livewire/VoteButton.php
@@ -2,49 +2,43 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 
 class VoteButton extends Component
 {
 
-    public $post;    // The forum post being voted on
-    public $isVoted; // Tracks whether the user has voted on this post
+    public $post;
+    public $isVoted;
+    public $class;
 
     public function mount($post)
     {
         $this->post = $post;
-        $this->isVoted = Auth::user()
-            ->votedPosts()
-            ->where('forum_post_id', $post->id)
-            ->exists(); // Check if the user already voted
+        $this->isVoted = User::find(Auth::id())->votedPosts()->where('forum_post_id', $post->id)->exists();
     }
 
     public function toggleVote()
-{
-    $user = Auth::user(); // Get the authenticated user
+    {
+        $user = User::find(Auth::id());
 
-    if ($this->isVoted) {
-        // User is unvoting
-        $user->votedPosts()->detach($this->post->id);
 
-        // Decrease the vote count
-        $this->post->decrement('votes');
-    } else {
-        // User is voting
-        $user->votedPosts()->attach($this->post->id, ['vote' => true]);
+        if ($this->isVoted) {
+            $user->votedPosts()->detach($this->post->id);
 
-        // Increase the vote count
-        $this->post->increment('votes');
+            $this->post->decrement('votes');
+        } else {
+            $user->votedPosts()->attach($this->post->id, ['vote' => true]);
+
+            $this->post->increment('votes');
+        }
+
+        $this->isVoted = !$this->isVoted;
     }
-
-    $this->isVoted = !$this->isVoted; // Toggle the state
-}
 
     public function render()
     {
         return view('livewire.vote-button');
     }
-
-
 }
